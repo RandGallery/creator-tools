3e24f7f176f5cb2aa209e3a8feea7743973d2922
Chris Antaki <ChrisAntaki@gmail.com>
2022-06-17 09:16:12 -0700

    Use new API URL

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const algosdk = require("algosdk");
 
 const TOKEN = ""; // Top secret, do not share.
-const SERVER = "https://algoexplorerapi.io";
+const SERVER = "https://node.algoexplorerapi.io";
 
 exports.client = new algosdk.Algodv2(TOKEN, SERVER, 443);
